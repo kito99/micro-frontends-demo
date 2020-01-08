99c7c594f37d8473aa07ee4abab345023f36ff65
kito99 <kito-public#virtua.com>
2020-01-08 11:41:43 -0500

    First draft of lab

---
+++ orchestrator-starter/README.md
@@ -0,0 +1,8 @@
+
+# orchestrator
+
+A single web page [index.html](./index.html) that uses the [Vaadin Router](https://vaadin.com/router) to load 
+different micro frontends.  
+
+It is part of `micro-frontends-demo`; see the [main README](../README.md) for details about the entire project and how 
+to run it. There is no build step for this app.
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 var app = express();
 
-app.use('/', express.static('orchestrator', {index: "index.html"}));
-app.use('/app-one', express.static('app-one/build'));
-app.use('/app-two', express.static('app-two/elements'));
-app.use('/app-three', express.static('app-three/elements'));
+app.use('/app/one', express.static('app-one/build'));
+app.use('/app/two', express.static('app-two/elements'));
+app.use('/app/three', express.static('app-three/elements'));
+app.use('*', express.static('orchestrator', {index: "index.html"}));
 
 app.listen(8000);
